e74117c95418418939d9fa663963ac8a3bc63364
scastanoh <93600096+scastanoh@users.noreply.github.com>
2024-11-13 11:35:24 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AsistenteApp from './AsistenteApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AsistenteApp />
+  </React.StrictMode>,
+)
